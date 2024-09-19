516d5c179b0c5e81c3e814db5b70d9060d9a790c
Piotr Roslaniec <p.roslaniec@gmail.com>
2024-09-19 13:27:55 +0200

    wip

--- parser_wasm/src/lib.rs
+++ parser_wasm/src/lib.rs
@@ -36,29 +36,21 @@ pub fn find_file(
     crr_file: &Path,
     ext_link_libraries: &[PathBuf],
     files_map: &HashMap<String, String>,
-) -> (bool, String, String, PathBuf, Vec<Report>) {
-    let mut path = String::new();
-    let mut src = String::new();
-    let mut crr_str_file = crr_file.to_path_buf();
+) -> Result<(String, String, PathBuf), Vec<Report>> {
     let mut reports = Vec::new();
 
     for aux in ext_link_libraries {
-        let mut p = aux.clone();
-        p.push(crr_file);
-        crr_str_file = p.clone();
-        log::info!("Trying to open file {}", crr_str_file.display());
-        match open_file(&crr_str_file, files_map) {
+        let p = aux.join(crr_file);
+        match open_file(&p, files_map) {
             Ok((new_path, new_src)) => {
-                path = new_path;
-                src = new_src;
-                return (true, path, src, crr_str_file, reports);
+                return Ok((new_path, new_src, p));
             }
             Err(e) => {
                 reports.push(e);
             }
         }
     }
-    (false, path, src, crr_str_file, reports)
+    Err(reports)
 }
 
 pub fn run_parser(
@@ -80,21 +72,30 @@ pub fn run_parser(
 
     while let Some(crr_file) = FileStack::take_next(&mut file_stack) {
         log::info!("Parsing file {}", crr_file.display());
-        let (found, path, src, crr_str_file, reports) =
-            find_file(&crr_file, &ext_link_libraries, files_map);
-        if !found {
-            log::error!("File {} not found", crr_str_file.display());
-            return Err((file_library, reports));
-        }
+        let (path, src, crr_str_file) = match find_file(&crr_file, &ext_link_libraries, files_map) {
+            Ok(result) => result,
+            Err(reports) => {
+                log::error!("File {} not found", crr_file.display());
+                return Err((file_library, reports));
+            }
+        };
+
         let file_id = file_library.add_file(path.clone(), src.clone());
         let program =
             parser_logic::parse_file(&src, file_id).map_err(|e| (file_library.clone(), e))?;
+
         if let Some(main) = program.main_component {
             main_components.push((file_id, main, program.custom_gates));
         }
-        includes_graph.add_node(crr_str_file, program.custom_gates, program.custom_gates_declared);
+
+        includes_graph.add_node(
+            crr_str_file.clone(),
+            program.custom_gates,
+            program.custom_gates_declared,
+        );
         let includes = program.includes;
         definitions.push((file_id, program.definitions));
+
         for include in includes {
             log::info!("Including file {}", include);
             let path_include = FileStack::add_include(
@@ -103,24 +104,26 @@ pub fn run_parser(
                 &link_libraries,
                 files_map,
             )
-                .map_err(|e| (file_library.clone(), vec![e]))?;
+            .map_err(|e| (file_library.clone(), vec![e]))?;
             includes_graph.add_edge(path_include).map_err(|e| (file_library.clone(), vec![e]))?;
         }
-        warnings.append(
-            &mut check_number_version(
+
+        warnings.extend(
+            check_number_version(
                 path.clone(),
                 program.compiler_version,
                 parse_number_version(version),
             )
-                .map_err(|e| (file_library.clone(), vec![e]))?,
+            .map_err(|e| (file_library.clone(), vec![e]))?,
         );
+
         if program.custom_gates {
             check_custom_gates_version(
-                path,
+                path.clone(),
                 program.compiler_version,
                 parse_number_version(version),
             )
-                .map_err(|e| (file_library.clone(), vec![e]))?
+            .map_err(|e| (file_library.clone(), vec![e]))?
         }
     }
 
@@ -130,80 +133,76 @@ pub fn run_parser(
         warnings.push(report);
         Err((file_library, warnings))
     } else if main_components.len() > 1 {
-        let report = produce_report_with_main_components(main_components);
+        let report = produce_report_with_main_components(&main_components);
         warnings.push(report);
         Err((file_library, warnings))
     } else {
-        let mut errors: ReportCollection = includes_graph.get_problematic_paths().iter().map(|path|
-            Report::error(
-                format!(
-                    "Missing custom templates pragma in file {} because of the following chain of includes {}",
-                    path.last().unwrap().display(),
-                    IncludesGraph::display_path(path)
-                ),
-                ReportCode::CustomGatesPragmaError,
-            )
-        ).collect();
+        let mut errors: ReportCollection = includes_graph
+            .get_problematic_paths()
+            .iter()
+            .map(|path| {
+                Report::error(
+                    format!(
+                        "Missing custom templates pragma in file {} because of the following chain of includes {}",
+                        path.last().unwrap().display(),
+                        IncludesGraph::display_path(path)
+                    ),
+                    ReportCode::CustomGatesPragmaError,
+                )
+            })
+            .collect();
+
         if !errors.is_empty() {
             warnings.append(&mut errors);
             Err((file_library, warnings))
         } else {
             let (main_id, main_component, custom_gates) = main_components.pop().unwrap();
-            let result_program_archive = ProgramArchive::new(
+            let mut program_archive = ProgramArchive::new(
                 file_library,
                 main_id,
                 main_component,
                 definitions,
                 custom_gates,
-            );
-            match result_program_archive {
-                Err((lib, mut rep)) => {
-                    warnings.append(&mut rep);
-                    Err((lib, warnings))
-                }
-                Ok(mut program_archive) => {
-                    let lib = program_archive.get_file_library().clone();
-                    let program_archive_result = apply_syntactic_sugar(&mut program_archive);
-                    match program_archive_result {
-                        Err(v) => {
-                            warnings.push(v);
-                            Err((lib, warnings))
-                        }
-                        Ok(_) => Ok((program_archive, warnings)),
-                    }
-                }
-            }
+            )
+            .map_err(|(lib, mut rep)| {
+                warnings.append(&mut rep);
+                (lib, warnings.clone())
+            })?;
+
+            apply_syntactic_sugar(&mut program_archive).map_err(|v| {
+                warnings.push(v);
+                (program_archive.get_file_library().clone(), warnings.clone())
+            })?;
+
+            Ok((program_archive, warnings))
         }
     }
 }
 
 fn produce_report_with_main_components(
-    main_components: Vec<(usize, (Vec<String>, Expression), bool)>,
+    main_components: &[(usize, (Vec<String>, Expression), bool)],
 ) -> Report {
-    let mut r = produce_report(ReportCode::MultipleMain, 0..0, 0);
-    for (idx, (i, exp, _)) in main_components.into_iter().enumerate() {
-        if idx > 0 {
-            r.add_secondary(
-                exp.1.get_meta().location.clone(),
-                i,
-                Some("Here is another main component".to_string()),
-            );
+    let mut report = produce_report(ReportCode::MultipleMain, 0..0, 0);
+    for (idx, (file_id, (_names, expression), _)) in main_components.iter().enumerate() {
+        let location = expression.get_meta().location.clone();
+        let message = if idx == 0 {
+            "This is a main component".to_string()
         } else {
-            r.add_primary(
-                exp.1.get_meta().location.clone(),
-                i,
-                "This is a main component".to_string(),
-            );
+            "Here is another main component".to_string()
+        };
+        if idx == 0 {
+            report.add_primary(location, *file_id, message);
+        } else {
+            report.add_secondary(location, *file_id, Some(message));
         }
     }
-    r
+    report
 }
 
-fn open_file(
-    path: &Path,
-    files_map: &HashMap<String, String>,
-) -> Result<(String, String), Report> {
-    let path_str = path.to_str().unwrap();
+fn open_file(path: &Path, files_map: &HashMap<String, String>) -> Result<(String, String), Report> {
+    let path_str = path.to_str().ok_or_else(|| {
+        produce_report_with_message(ReportCode::FileOs, format!("Invalid path {:?}", path))
+    })?;
     if let Some(src) = files_map.get(path_str) {
         Ok((path_str.to_string(), src.clone()))
     } else {
@@ -225,11 +224,7 @@ fn check_number_version(
     version_compiler: Version,
 ) -> Result<ReportCollection, Report> {
     if let Some(required_version) = version_file {
-        if required_version.0 == version_compiler.0
-            && (required_version.1 < version_compiler.1
-            || (required_version.1 == version_compiler.1
-            && required_version.2 <= version_compiler.2))
-        {
+        if required_version <= version_compiler {
             Ok(vec![])
         } else {
             Err(produce_compiler_version_report(file_path, required_version, version_compiler))
@@ -247,24 +242,16 @@ fn check_custom_gates_version(
 ) -> Result<(), Report> {
     let custom_gates_version: Version = (2, 0, 6);
     let version_to_check = version_file.unwrap_or(version_compiler);
-    if version_to_check.0 < custom_gates_version.0
-        || (version_to_check.0 == custom_gates_version.0
-        && version_to_check.1 < custom_gates_version.1)
-        || (version_to_check.0 == custom_gates_version.0
-        && version_to_check.1 == custom_gates_version.1
-        && version_to_check.2 < custom_gates_version.2)
-    {
+    if version_to_check >= custom_gates_version {
+        Ok(())
+    } else {
         let report = Report::error(
             format!(
                 "File {} requires at least version {:?} to use custom templates (currently {:?})",
-                file_path,
-                custom_gates_version,
-                version_to_check
+                file_path, custom_gates_version, version_to_check
             ),
             ReportCode::CustomGatesVersionError,
         );
         Err(report)
-    } else {
-        Ok(())
     }
 }
